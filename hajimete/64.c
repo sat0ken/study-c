c23d3c86be81c9f33bfcaf2faae3cfb0fe73f56e
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-12-14 14:07:31 +0900

    add files

---
+++ hajimete/64.c
@@ -0,0 +1,10 @@
+main()
+{
+    int a = 123;
+    float b = 123.456;
+    char c = 'A';
+    static char cstr[] = "C Program !!";
+
+    printf("a --> %d b --> %f c --> %c\n", a, b, c);
+    printf("string cstr --> %s\n", cstr);
+}
---
+++ hajimete/65.c
@@ -0,0 +1,10 @@
+main()
+{
+    int age;
+    char name[20];
+
+    printf("\nAge ? "); scanf("%d", &age);
+    printf("name ? "); scanf("%s", name);
+
+    printf("\n[ %d %s ]\n", age, name);
+}
