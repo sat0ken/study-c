113ccc8d254a04fba9afaeba818b02e6e9fb0fb9
sat0ken <15720506+sat0ken@users.noreply.github.com>
2021-01-08 22:23:18 +0900

    add files

---
+++ hajimete/108.c
@@ -0,0 +1,19 @@
+main()
+{
+    int ia, ib;
+    float fa, fb;
+    char ca, cb;
+
+    ia = 5 / 3;
+    ib = 5 % 3;
+
+    fa = 5 /3;
+    fb = 5.0 / 3.0;
+
+    ca = '@' + 1;
+    cb = 'G' - 1;
+
+    printf("int     ia, ib -->%d, %d\n", ia, ib);
+    printf("float   fa, fb -->%d, %d\n", fa, fb);
+    printf("char    ca, cb -->%d, %d\n", ca, cb);
+}
